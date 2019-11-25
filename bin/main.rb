df775af249c9db0874537897442f2aa6f05a1513
Eleazar Meza <bairesdev_emeza@iseatz.com>
2019-11-25 11:36:52 -0400

    Implemented basic user interface logic

--- bin/main.rb
+++ bin/main.rb
@@ -2,20 +2,20 @@
 
 # game = TicTacToe.new
 
-# while !game.finished? 
+while true #!game.finished?
+  #game.display
+  puts "Game board is displayed..."
+  print "Current player, your next move choose a number (1-9)?: "
+  move = gets.chomp.to_i
+  #puts "Invalid move" unless game.play(move)
+  puts "Your moved is played"
 
-#   game.display
-
-#   puts "Current player, your next move choose a number: "
-
-#   move = get.chomp.to_i
-
-#   puts "Invalid move" unless game.play(move) 
-
-# end
+  # Until the game logic is implemented, we just simulate the game finishing at random
+  break if rand > 0.7
+end
 
 # if game.winner
 #   "Winner is current player"
-# else 
+# else
 #   "Game finished in a draw"
-
+puts rand > 0.5 ? "The winner is X" : "Game ended in a draw"
