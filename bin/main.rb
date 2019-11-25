47264289aaa6f9eadeb7338d491fb8710e916185
Ermin Cahtarevic <erminc@windowslive.com>
2019-11-25 16:26:10 +0100

    adding basic outlines

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,21 @@
 #!/usr/bin/env ruby
 
-puts "Hello World!"
+# game = TicTacToe.new
+
+# while !game.finished? 
+
+#   game.display
+
+#   puts "Current player, your next move choose a number: "
+
+#   move = get.chomp.to_i
+
+#   puts "Invalid move" unless game.play(move) 
+
+# end
+
+# if game.winner
+#   "Winner is current player"
+# else 
+#   "Game finished in a draw"
+
