5b17ff27ad543f7f2c6deb021f2c4edda70320db
Eleazar Meza <bairesdev_emeza@iseatz.com>
2019-11-25 15:38:06 -0400

    Updated user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,21 +1,40 @@
 #!/usr/bin/env ruby
 
-# game = TicTacToe.new
-
-while true #!game.finished?
-  #game.display
-  puts "Game board is displayed..."
-  print "Current player, your next move choose a number (1-9)?: "
-  move = gets.chomp.to_i
-  #puts "Invalid move" unless game.play(move)
-  puts "Your moved is played"
-
-  # Until the game logic is implemented, we just simulate the game finishing at random
-  break if rand > 0.7
+def valid_move?(move)
+  (1..9).include? move
 end
 
-# if game.winner
-#   "Winner is current player"
-# else
-#   "Game finished in a draw"
-puts rand > 0.5 ? "The winner is X" : "Game ended in a draw"
+def winning_move?
+  rand > 0.7
+end
+
+def draw_move?
+  rand > 0.7
+end
+
+def display_board
+  puts "Game board is displayed"
+end
+
+current_player = 'X'
+
+display_board
+
+while true
+  print "Player #{current_player}, your next move choose a number (1-9)?: "
+  move = gets.chomp.to_i
+  if valid_move?(move)
+    display_board
+    if winning_move?
+      puts "Player #{current_player} has won the game!"
+      break
+    elsif draw_move?
+      puts "Game ended in a draw"
+      break
+    else
+      current_player = current_player == "X" ? "O" : "X"
+    end
+  else
+    puts "Invalid move!"
+  end
+end
